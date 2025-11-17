f73671b41861bb9d2eb606fa7c0fd0f08137b262
Yash Jadwani <yash.jadwani1998@gmail.com>
2025-11-17 08:36:20 +0000

    Added new features and trained new XGb classifer on new features with improved recall

--- db_connection.py
+++ db_connection.py
@@ -6,10 +6,9 @@ import pandas as pd
 # Load environment variables
 load_dotenv()
 
-def connect_to_db():
-    return create_engine(db_url,connect_args={'connect_timeout': 60})
-
-
 db_url = (
         f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
         f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}")
+
+def connect_to_db():
+    return create_engine(db_url,connect_args={'connect_timeout': 60})
